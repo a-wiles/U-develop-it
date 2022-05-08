37f577fd5c577b32ad960a6eced02d620840e5cc
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-05-08 16:40:16 -0400

    Set up query

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'HotDo932',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req, res) => {
     res.status(404).end();
